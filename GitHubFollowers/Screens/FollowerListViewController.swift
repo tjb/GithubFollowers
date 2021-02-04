ba1ab63fad28c6994c74967425789257d10fd21c
tyler <tjbobella@gmail.com>
2021-02-04 00:15:16 -0600

    network manager! lets make some api calls my dudes

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class FollowerListViewController: UIViewController {
     
-    var username: String?
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMsg) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMsg!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Followers.count = \(followers.count)")
+        }
     }
     
     
